3f52fe831fd7ab23323d51f919899b5c9362ae12
sonupandey19 <pandeysonu902@gmail.com>
2023-06-13 08:03:54 +0530

    first commit

--- app/src/main/java/com/example/cryptologinandsignup/SignUpActivity.java
+++ app/src/main/java/com/example/cryptologinandsignup/SignUpActivity.java
@@ -2,13 +2,26 @@ package com.example.cryptologinandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
